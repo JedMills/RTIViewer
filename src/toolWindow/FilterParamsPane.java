6515af2bc036d002a8ba230a80f96f116575e5ec
Jed <jm729@exeter.ac.uk>
2017-06-01 19:36:01 +0100

    Added the sliders for the diffuse gain and specular enhancement parameters. Changed the xpos and ypos entry boxes to spinners to add functionality.

--- src/toolWindow/FilterParamsPane.java
+++ src/toolWindow/FilterParamsPane.java
@@ -1,5 +1,14 @@
 package toolWindow;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 /**
@@ -7,6 +16,175 @@ import javafx.scene.layout.Pane;
  */
 public class FilterParamsPane extends Pane {
 
+    private ToolWindow toolWindow;
+    private ComboBox<String> filterChoiceBox;
+    private Scene parent;
+    private int width;
+    private int height;
+
+    public static final double INITIAL_DIFF_GAIN_VAL = 0.0;
+    public static final double INITIAL_DIFF_COLOUR_VAL = 0.0;
+    public static final double INITIAL_SPEC_VAL = 0.0;
+    public static final double INITIAL_HIGHLIGHT_VAL = 0.0;
+
+    private Label gainLabel;
+    private Slider gainSlider;
+    private Spinner<Double> gainSpinner;
+
+    private Label seColourLabel;
+    private Slider seColourSlider;
+    private Spinner<Double> seColourSpinner;
+
+    private Label seSpecLabel;
+    private Slider seSpecSlider;
+    private Spinner<Double> seSpecSpinner;
+
+    private Label seHighlightLabel;
+    private Slider seHighlightSlider;
+    private Spinner<Double> seHighlightSpinner;
+
+    private Control[] allControls;
+
+    public FilterParamsPane(ToolWindow toolWindow, Scene parent, ComboBox<String> filterChoiceBox) {
+        super();
+        this.filterChoiceBox = filterChoiceBox;
+        this.parent = parent;
+        this.toolWindow = toolWindow;
+
+        setWidth(parent.getWidth());
+        setHeight(200);
+        createComponents();
+
+         allControls = new Control[]{gainLabel, gainSlider, gainSpinner, seColourLabel, seColourSlider,
+                seColourSpinner, seSpecLabel, seSpecSlider, seSpecSpinner,
+                seHighlightLabel, seHighlightSlider, seHighlightSpinner};
+
+        hideAllItems();
+    }
+
+    public FilterParamsPane(Node... children) {
+        super(children);
+    }
+
+    private void createComponents(){
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(20);
+        gridPane.setVgap(20);
+        createDiffGainComponents(gridPane);
+        createSpecularEnhanceComponents(gridPane);
+
+        getChildren().add(gridPane);
+    }
 
 
+    private void createDiffGainComponents(GridPane gridPane){
+        gainLabel = new Label("Gain");
+
+        gainSlider = new Slider(0.0, 100.0, INITIAL_DIFF_GAIN_VAL);
+        gainSpinner = new Spinner<>(0.0, 100.0, INITIAL_DIFF_GAIN_VAL, 1.0);
+        setupSliderSpinnerPair(gainSlider, gainSpinner, "Invalid entry for diffuse gain spinner.",
+                ToolWindow.GlobalParam.DIFF_GAIN);
+
+        GridPane.setConstraints(gainLabel, 0, 0);
+        GridPane.setConstraints(gainSlider, 1, 0);
+        GridPane.setConstraints(gainSpinner, 2, 0);
+
+        gridPane.getChildren().addAll(gainLabel, gainSlider, gainSpinner);
+    }
+
+
+    private void createSpecularEnhanceComponents(GridPane gridPane){
+        seColourLabel = new Label("Diffuse colour");
+        seColourSlider = new Slider(0.0, 100.0, INITIAL_DIFF_COLOUR_VAL);
+        seColourSpinner = new Spinner<>(0.0, 100.0, INITIAL_DIFF_COLOUR_VAL, 1.0);
+        setupSliderSpinnerPair(seColourSlider, seColourSpinner, "Invalid entry for diffuse colour spinner.",
+                ToolWindow.GlobalParam.DIFF_COLOUR);
+
+        seSpecLabel = new Label("Specularity");
+        seSpecSlider = new Slider(0.0, 100.0, INITIAL_SPEC_VAL);
+        seSpecSpinner = new Spinner<>(0.0, 100.0, INITIAL_SPEC_VAL, 1.0);
+        setupSliderSpinnerPair(seSpecSlider, seSpecSpinner, "Invalid entry for specularity spinner.",
+                ToolWindow.GlobalParam.SPECULARITY);
+
+        seHighlightLabel = new Label("Highlight size");
+        seHighlightSlider = new Slider(0.0, 100.0, INITIAL_HIGHLIGHT_VAL);
+        seHighlightSpinner = new Spinner<>(0.0, 100.0, INITIAL_HIGHLIGHT_VAL, 1.0);
+        setupSliderSpinnerPair(seHighlightSlider, seHighlightSpinner, "Invalid entry for highlight size spinner.",
+                ToolWindow.GlobalParam.HIGHTLIGHT_SIZE);
+
+        GridPane.setConstraints(seColourLabel, 0, 0);
+        GridPane.setConstraints(seColourSlider, 1, 0);
+        GridPane.setConstraints(seColourSpinner, 2, 0);
+
+        GridPane.setConstraints(seSpecLabel, 0, 1);
+        GridPane.setConstraints(seSpecSlider, 1, 1);
+        GridPane.setConstraints(seSpecSpinner, 2, 1);
+
+        GridPane.setConstraints(seHighlightLabel, 0, 2);
+        GridPane.setConstraints(seHighlightSlider, 1, 2);
+        GridPane.setConstraints(seHighlightSpinner, 2, 2);
+
+        gridPane.getChildren().addAll(seColourLabel,    seColourSlider,     seColourSpinner,
+                                      seSpecLabel,      seSpecSlider,       seSpecSpinner,
+                                      seHighlightLabel, seHighlightSlider,  seHighlightSpinner);
+    }
+
+
+    private void setupSliderSpinnerPair(Slider slider, Spinner spinner, String warningText,
+                                        ToolWindow.GlobalParam globalParam){
+
+        slider.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                spinner.getEditor().setText(String.valueOf(slider.getValue()));
+                toolWindow.setGlobalVal(globalParam, slider.getValue());
+            }
+        });
+        spinner.setEditable(true);
+        spinner.setPrefWidth(75);
+        spinner.getEditor().setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try{
+                    Double value = Double.parseDouble(spinner.getEditor().getText());
+                    slider.setValue(value);
+                    toolWindow.setGlobalVal(globalParam, value);
+                }catch(NumberFormatException e){
+                    toolWindow.entryAlert.setContentText(warningText);
+                    toolWindow.entryAlert.showAndWait();
+                }
+            }
+        });
+        spinner.valueProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                Double value = Double.parseDouble(spinner.getEditor().getText());
+                slider.setValue(value);
+                toolWindow.setGlobalVal(globalParam, value);
+            }
+        });
+    }
+
+    public void setCurrentFilter(String filterType){
+        hideAllItems();
+        if(filterType.equals("Default view")){}
+        else if(filterType.equals("Normals visualisation")){}
+        else if(filterType.equals("Diffuse gain")){
+            gainLabel.setVisible(true);
+            gainSlider.setVisible(true);
+            gainSpinner.setVisible(true);
+        }
+        else if(filterType.equals("Specular enhancement")){
+            for(int i = 3; i <= 11; i++){
+                allControls[i].setVisible(true);
+            }
+        }
+
+    }
+
+    private void hideAllItems(){
+        for(Control control : allControls){
+            control.setVisible(false);
+        }
+    }
 }
--- src/toolWindow/ToolWindow.java
+++ src/toolWindow/ToolWindow.java
@@ -1,12 +1,13 @@
 package toolWindow;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.effect.Light;
@@ -24,16 +25,30 @@ import utils.Utils;
  */
 public class ToolWindow extends Application {
 
-    private static Stage primaryStage;
-    private static Light.Point light;
-    private static Circle circle;
-    private static Utils.Vector2f globalLightPos;
-    private static NumericField xPosBox, yPosBox;
+    private Stage primaryStage;
+    private Light.Point light;
+    private Circle circle;
+
+    private Utils.Vector2f globalLightPos = new Utils.Vector2f(0, 0);
+    private double globalDiffGainVal = FilterParamsPane.INITIAL_DIFF_GAIN_VAL;
+    private double globalDiffColourVal = FilterParamsPane.INITIAL_DIFF_COLOUR_VAL;
+    private double globalSpecularityVal = FilterParamsPane.INITIAL_SPEC_VAL;
+    private double globalHighlightSizeVal = FilterParamsPane.INITIAL_HIGHLIGHT_VAL;
+
+    private Spinner<Double> xPosBox, yPosBox;
+    private FilterParamsPane paramsPane;
+
+    private enum LightEditor{CIRCLE, XSPINNER, YSPINNER;}
+    public enum GlobalParam{DIFF_GAIN, DIFF_COLOUR, SPECULARITY, HIGHTLIGHT_SIZE;}
+
+    public Alert entryAlert;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        ToolWindow.primaryStage = primaryStage;
+        primaryStage = primaryStage;
         primaryStage.setTitle("RTI Viewer");
+
+        createEntryAlert();
 
         Scene scene = createScene(primaryStage);
         primaryStage.setScene(scene);
@@ -41,8 +56,12 @@ public class ToolWindow extends Application {
         primaryStage.show();
     }
 
+    private void createEntryAlert(){
+        entryAlert = new Alert(Alert.AlertType.WARNING);
+        entryAlert.setTitle("Invalid Entry");
+    }
 
-    private static Scene createScene(Stage primaryStage){
+    private Scene createScene(Stage primaryStage){
         FlowPane flowPane = new FlowPane();
         Scene scene = new Scene(flowPane, 400, 600);
 
@@ -55,13 +74,17 @@ public class ToolWindow extends Application {
         ComboBox<String> comboBox = createFilterChoiceBox();
         flowPane.getChildren().add(comboBox);
 
+        paramsPane = new FilterParamsPane(this, scene, comboBox);
+        flowPane.getChildren().add(paramsPane);
+
         flowPane.setMargin(lightControlGroup, new Insets(20, 0, 0, 20));
         flowPane.setMargin(comboBox, new Insets(20, 0, 0, (scene.getWidth() / 2) - (comboBox.getPrefWidth() / 2) ));
+        flowPane.setMargin(paramsPane, new Insets(20, 0, 0, 20));
         return scene;
     }
 
 
-    private static MenuBar createMenuBar(Stage primaryStage){
+    private MenuBar createMenuBar(Stage primaryStage){
         MenuBar menuBar = new MenuBar();
         Menu menuFile = new Menu("File");
         MenuItem open = new MenuItem("Open");
@@ -76,7 +99,7 @@ public class ToolWindow extends Application {
     }
 
 
-    private static Pane createLightControl(Stage primaryStage, FlowPane parent){
+    private Pane createLightControl(Stage primaryStage, FlowPane parent){
         Pane pane = new Pane();
 
         Circle circle = createSpecularBall(primaryStage, pane);
@@ -93,27 +116,57 @@ public class ToolWindow extends Application {
         yPosLabel.setLayoutX(2 * circle.getRadius() + 40);
         yPosLabel.setLayoutY(70);
 
-        xPosBox = new NumericField("0.0") {
-            @Override
-            public void updateAction(String stringValue) {
-                updateGlobalLightPos(new Utils.Vector2f(Float.parseFloat(stringValue), globalLightPos.y));
-                updateLightControls();
-            }
-        };
-        xPosBox.setPrefWidth(60);
+        xPosBox = new Spinner<Double>(-1.0, 1.0, 0.0, 0.01);
+        xPosBox.setEditable(true);
+        xPosBox.setPrefWidth(80);
         xPosBox.setLayoutX(2 * circle.getRadius() + 90);
         xPosBox.setLayoutY(37);
-
-        yPosBox = new NumericField("0.0") {
+        xPosBox.getEditor().setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void updateAction(String stringValue) {
-                updateGlobalLightPos(new Utils.Vector2f(globalLightPos.x, Float.parseFloat(stringValue)));
-                updateLightControls();
+            public void handle(ActionEvent event) {
+                try {
+                    Float value = Float.parseFloat(xPosBox.getEditor().getText());
+                    updateGlobalLightPos(new Utils.Vector2f(value, globalLightPos.y), LightEditor.XSPINNER);
+                }catch(NumberFormatException e){
+                    entryAlert.setContentText("Invalid entry for light X position.");
+                    entryAlert.showAndWait();
+                }
             }
-        };
-        yPosBox.setPrefWidth(60);
+        });
+
+       xPosBox.valueProperty().addListener(new ChangeListener<Double>() {
+           @Override
+           public void changed(ObservableValue<? extends Double> observable, Double oldValue, Double newValue) {
+               Float value = Float.parseFloat(xPosBox.getEditor().getText());
+               updateGlobalLightPos(new Utils.Vector2f(value, globalLightPos.y), LightEditor.XSPINNER);
+           }
+       });
+
+        yPosBox = new Spinner<Double>(-1.0, 1.0, 0.0, 0.01);
+        yPosBox.setEditable(true);
+        yPosBox.setPrefWidth(80);
         yPosBox.setLayoutX(2 * circle.getRadius() + 90);
         yPosBox.setLayoutY(67);
+        yPosBox.getEditor().setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Float value = Float.parseFloat(yPosBox.getEditor().getText());
+                    updateGlobalLightPos(new Utils.Vector2f(globalLightPos.x, value), LightEditor.YSPINNER);
+                }catch(NumberFormatException e){
+                    entryAlert.setContentText("Invalid entry for light Y position.");
+                    entryAlert.showAndWait();
+                }
+            }
+        });
+
+        yPosBox.valueProperty().addListener(new ChangeListener<Double>() {
+            @Override
+            public void changed(ObservableValue<? extends Double> observable, Double oldValue, Double newValue) {
+                Float value = Float.parseFloat(yPosBox.getEditor().getText());
+                updateGlobalLightPos(new Utils.Vector2f(globalLightPos.x, value), LightEditor.YSPINNER);
+            }
+        });
 
         pane.getChildren().addAll(xPosLabel, xPosBox, yPosLabel, yPosBox);
 
@@ -121,7 +174,7 @@ public class ToolWindow extends Application {
     }
 
 
-    private static Circle createSpecularBall(Stage primaryStage, Pane parentGroup){
+    private Circle createSpecularBall(Stage primaryStage, Pane parentGroup){
         circle = new Circle();
         circle.setRadius(100.0);
         circle.setFill(Paint.valueOf("#d3d3d3"));
@@ -146,8 +199,8 @@ public class ToolWindow extends Application {
                         && event.getY() < circle.getCenterY() + circle.getRadius()) {
                     light.setX(event.getX() + 100);
                     light.setY(event.getY() + 100);
-                    calculateNormalisedLight(light, circle);
-                    updateLightControls();
+                    Utils.Vector2f normalised = calculateNormalisedLight(light, circle);
+                    updateGlobalLightPos(normalised, LightEditor.CIRCLE);
                 }
             }
         });
@@ -168,8 +221,8 @@ public class ToolWindow extends Application {
                     light.setX(x);
                     light.setY(y);
                 }
-                calculateNormalisedLight(light, circle);
-                updateLightControls();
+                Utils.Vector2f normalised = calculateNormalisedLight(light, circle);
+                updateGlobalLightPos(normalised, LightEditor.CIRCLE);
             }
         });
 
@@ -177,29 +230,36 @@ public class ToolWindow extends Application {
     }
 
 
-    private static void updateGlobalLightPos(Utils.Vector2f newLight){
+    private void updateGlobalLightPos(Utils.Vector2f newLight, LightEditor source){
         if(newLight.length() > 1.0) {
             globalLightPos = newLight.normalise();
         }else{
             globalLightPos = newLight;
         }
+        updateLightControls(source);
     }
 
-    private static void updateLightControls(){
-        light.setX(globalLightPos.x * circle.getRadius() + circle.getLayoutX());
-        light.setY(globalLightPos.y * circle.getRadius() + circle.getLayoutY());
-        xPosBox.setText(String.valueOf(globalLightPos.x));
-        yPosBox.setText(String.valueOf(globalLightPos.y));
+    private void updateLightControls(LightEditor source){
+        if(!source.equals(LightEditor.CIRCLE)) {
+            light.setX(globalLightPos.x * circle.getRadius() + circle.getLayoutX());
+            light.setY(globalLightPos.y * circle.getRadius() + circle.getLayoutY());
+        }
+        if(!source.equals(LightEditor.XSPINNER)){
+            xPosBox.getValueFactory().setValue((double)globalLightPos.x);
+        }
+        else if(!source.equals(LightEditor.YSPINNER)){
+            yPosBox.getValueFactory().setValue((double)globalLightPos.y);
+        }
     }
 
-    private static void calculateNormalisedLight(Light.Point light, Circle circle){
+    private Utils.Vector2f calculateNormalisedLight(Light.Point light, Circle circle){
         double x = (light.getX() - circle.getRadius()) / circle.getRadius();
         double y = (light.getY() - circle.getRadius()) / circle.getRadius();
-        updateGlobalLightPos(new Utils.Vector2f((float)x, (float)y));
+        return new Utils.Vector2f((float)x, (float)y);
     }
 
 
-    private static ComboBox<String> createFilterChoiceBox(){
+    private ComboBox<String> createFilterChoiceBox(){
         ObservableList<String> options = FXCollections.observableArrayList(
                 "Default view",
                 "Normals visualisation",
@@ -209,6 +269,12 @@ public class ToolWindow extends Application {
         ComboBox<String> comboBox = new ComboBox<>(options);
         comboBox.getSelectionModel().select(0);
         comboBox.setPrefWidth(180);
+        comboBox.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                paramsPane.setCurrentFilter(comboBox.getSelectionModel().getSelectedItem());
+            }
+        });
 
         //return selectorBox;
         return comboBox;
@@ -217,5 +283,28 @@ public class ToolWindow extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public double getGlobalDiffGainVal() {
+        return globalDiffGainVal;
+    }
+
+    public void setGlobalDiffGainVal(double globalDiffGainVal) {
+        this.globalDiffGainVal = globalDiffGainVal;
+    }
+
+    public double getGlobalDiffColourVal() {
+        return globalDiffColourVal;
+    }
+
+    public void setGlobalDiffColourVal(double globalDiffColourVal) {
+        this.globalDiffColourVal = globalDiffColourVal;
+    }
+
+    public void setGlobalVal(GlobalParam param, double value){
+        if(param.equals(GlobalParam.DIFF_GAIN)){globalDiffGainVal = value;}
+        else if(param.equals(GlobalParam.DIFF_COLOUR)){globalDiffColourVal = value;}
+        else if(param.equals(GlobalParam.SPECULARITY)){globalSpecularityVal = value;}
+        else if(param.equals(GlobalParam.HIGHTLIGHT_SIZE)){globalHighlightSizeVal = value;}
     }
 }
